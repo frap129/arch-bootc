467d73fccc0b0011e05fe289ef62e4a163b51ed3
Joe Maples <joe@maples.dev>
2025-02-12 15:57:37 -0500

    feat: Add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME="arch-bootc"
+
+(
+  cd pkgbuilds/bootc
+  makepkg -fcCs
+)
+
+(
+  cd pkgbuilds/bootupd
+  makepkg -fcCs
+)
+
+podman build . -t $IMAGE_NAME --net=host --cap-add sys_admin --cap-add mknod
